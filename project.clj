7bf2fcbf0e3f63867da8e521354848e3fb6e0b7f
nathants <me@nathants.com>
2016-11-18 23:28:10 -0800

    travis

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [aleph "0.4.1"]
                  [compojure "1.6.0-beta1"]
                  [cheshire "5.6.3"]
-                 [snapshots/confs "master-b1231ed"]]
+                 [snapshots/confs "master-3503fca"]]
   :uberjar-name "spq.jar"
   :plugins [[s3-wagon-private "1.3.0-alpha2"]]
   :repositories {"snapshots" {:url ~(System/getenv "S3P_SNAPSHOTS_URL")
