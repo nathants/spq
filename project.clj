cba0d06afda1f6b5d914d88cffd2eed9362b5f29
nathants <me@nathants.com>
2016-11-20 21:40:18 -0800

    bump confs

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [aleph "0.4.1"]
                  [compojure "1.6.0-beta1"]
                  [cheshire "5.6.3"]
-                 [snapshots/confs "master-3503fca"]]
+                 [snapshots/confs "master-22e882e"]]
   :uberjar-name "spq.jar"
   :plugins [[s3-wagon-private "1.3.0-alpha2"]]
   :repositories {"snapshots" {:url ~(System/getenv "S3P_SNAPSHOTS_URL")
