8a6845ab91d35b322d1334fbbb79f4b49953a988
nathants <me@nathants.com>
2016-11-20 21:07:17 -0800

    parameterize lib build scripts

--- bin/build_libs.sh
+++ bin/build_libs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -eux
 
+# builds jars and pushes them to s3 to be pulled via leiningen's s3-wagons-private
+
+# usage: bash build_libs.sh
+# usage: repos=repo-foo hash=asdf123 bash build_libs.sh
+
 # requires env vars like:
 # export S3P_SNAPSHOTS_URL=s3p://$BUCKET/software/
 # export S3_SNAPSHOTS_URL=s3://$BUCKET/software/snapshots
 
-for repo in clj-schema clj-confs; do
+build() {
+    repo=$1
     cd $(mktemp -d)
     tmp_path=$(pwd)
     git clone git@github.com:nathants/${repo}
@@ -13,9 +19,16 @@ for repo in clj-schema clj-confs; do
     name=$(echo ${repo} | cut -d- -f2)
     lein jar
     lein pom
-    latest_hash=$(git log --format=%h|head -n1)
-    aws s3 cp pom.xml $S3_SNAPSHOTS_URL/${name}/master-${latest_hash}/${name}-master-${latest_hash}.pom
-    aws s3 cp target/*.jar $S3_SNAPSHOTS_URL/${name}/master-${latest_hash}/${name}-master-${latest_hash}.jar
+    hash=${hash:-$(git log --format=%h|head -n1)}
+    hash=$(echo $hash|head -c7)
+    aws s3 cp pom.xml $S3_SNAPSHOTS_URL/${name}/master-${hash}/${name}-master-${hash}.pom
+    aws s3 cp target/*.jar $S3_SNAPSHOTS_URL/${name}/master-${hash}/${name}-master-${hash}.jar
     cd /tmp
     rm -rf ${tmp_path}
+}
+
+repos=${repos:-"clj-schema clj-confs"}
+
+for repo in $repos; do
+    build $repo
 done
--- bin/install_libs.sh
+++ bin/install_libs.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 set -eux
 
-for repo in clj-schema clj-confs; do
+# install jars into local ~/.m2 repository for use with leiningen
+
+# usage: bash install_libs.sh
+# usage: repos=repo-foo hash=asdf123 bash install_libs.sh
+
+install() {
+    repo=$1
     cd $(mktemp -d)
     tmp_path=$(pwd)
     git clone https://github.com/nathants/${repo}
     cd ${repo}
-    latest_hash=$(git log --format=%h|head -n1)
-    sed -r -i "s:defproject ([^ ]+) .*:defproject snapshots/\1 \"master-${latest_hash}\":" project.clj
+    hash=${hash:-$(git log --format=%h|head -n1)}
+    hash=$(echo $hash|head -c7)
+    sed -r -i "s:defproject ([^ ]+) .*:defproject snapshots/\1 \"master-${hash}\":" project.clj
     lein install
     cd /tmp
     rm -rf ${tmp_path}
+
+}
+
+repos=${repos:-"clj-schema clj-confs"}
+
+for repo in $repos; do
+    install $repo
 done
