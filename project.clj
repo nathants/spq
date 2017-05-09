4ad3fd3eb65f7ba481ee11e4c5432c88895e3a95
nathants <me@nathants.com>
2017-05-08 21:03:30 -0700

    bump dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "simple persistent queue"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/timbre "4.7.4"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [com.taoensso/timbre "4.10.0"]
                  [factual/durable-queue "0.1.5"]
-                 [clj-http "3.3.0"]
-                 [aleph "0.4.1"]
-                 [compojure "1.6.0-beta3"]
+                 [clj-http "3.5.0"]
+                 [aleph "0.4.3"]
+                 [compojure "1.6.0"]
                  [cheshire "5.6.3"]
-                 [snapshots/confs "master-a8ee32c"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [snapshots/confs "master-a8ee32c"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :uberjar-name "spq.jar"
   :plugins [[s3-wagon-private "1.3.0-alpha2"]]
   :repositories {"snapshots" {:url ~(System/getenv "S3P_SNAPSHOTS_URL")
